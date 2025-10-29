18a62f5ad09235e8252e161eaa5ecfb5d7796b7e
bikash-yadav20 <nishantyadav2488@gmail.com>
2025-10-29 19:40:19 +0530

    DB connected

---
+++ src/index.js
@@ -0,0 +1,102 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async() =>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.error("ERROR:", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server started on port ${process.env.PORT}`);
+        });
+
+    } catch(error){
+        console.error("ERROR:", error);
+        throw er
+    }
+})()
+*/
